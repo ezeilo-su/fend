b8679064ad8afbb5ac70a7f03879178f668fd843
ezeilo-su <ezeilo.su@gmail.com>
2021-10-18 00:01:51 +0100

    Display message moderation status

--- src/CommentsList.jsx
+++ src/CommentsList.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 
 export default function CommentsList({ comments }) {
   const renderedComments = comments.map((comment) => {
-    return <li key={comment.id}>{comment.content}</li>;
+    let content;
+
+    if (comment.status === 'approved') content = comment.content;
+    if (comment.status === 'rejected')
+      content = 'This comment has been rejected';
+    if (comment.status === 'pending')
+      content = 'This comment is awaiting moderation';
+
+    return <li key={comment.id}>{content}</li>;
   });
 
   return <ul>{renderedComments}</ul>;
