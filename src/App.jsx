708cd544010614d43c712cf6ee46b6ca8d21dac3
ezeilo-su <ezeilo.su@gmail.com>
2021-11-12 11:59:05 +0100

    Rename route paths to /posts/create for a unique route path

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,9 @@ export default function App() {
     <div className='container'>
       <h1>Create Post</h1>
       <PostCreate />
-      <hr/>
+      <hr />
       <h1>Posts</h1>
-      <PostList/>
+      <PostList />
     </div>
   );
 }
